62448a0d0f289c1143ffd5821a814cd693e9e054
1625094153@qq.com <40972854+AsamuQ@users.noreply.github.com>
2018-07-11 19:40:58 +0800

    轮播图修改
    自动轮播和滑动切换仍有问题

--- app/src/main/java/com/example/yuechu/IndexActivity.java
+++ app/src/main/java/com/example/yuechu/IndexActivity.java
@@ -1,7 +1,6 @@
 package com.example.yuechu;
 
 import android.app.Activity;
-import android.media.ImageWriter;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -12,56 +11,58 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.ViewSwitcher;
-import android.view.ViewGroup.LayoutParams;
+import android.widget.ViewSwitcher.ViewFactory;
 
-public class MainActivity extends Activity implements View.OnTouchListener{
+import static java.lang.Thread.sleep;
+
+public class IndexActivity extends Activity implements ViewFactory,View.OnTouchListener {
     private ImageSwitcher imageSwitcher;
+    private LinearLayout linearLayout;
     private int x=0;
     private int index=0;
     private int[] images;
     private float downX;                        //按下点的X坐标
-    private boolean isSlip=false;
+    private ImageView[] tips;
 
 
     Handler handler=new Handler(){
         @Override
         public void handleMessage(Message msg){
             if(msg.what==111){
+                imageSwitcher.setOutAnimation(AnimationUtils.loadAnimation(IndexActivity.this, R.anim.right_in));
                 imageSwitcher.setImageResource(images[msg.arg1]);
-                imageSwitcher.setInAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_in));
-                imageSwitcher.setOutAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_out));
-
             }
             else if (msg.what==112){
+                imageSwitcher.setInAnimation(AnimationUtils.loadAnimation(IndexActivity.this,R.anim.left_in));
                 imageSwitcher.setImageResource(images[msg.arg1]);
-                imageSwitcher.setInAnimation(AnimationUtils.loadAnimation(MainActivity.this,R.anim.left_in));
-                imageSwitcher.setOutAnimation(AnimationUtils.loadAnimation(getApplication(), R.anim.right_out));
             }
             else if (msg.what==113){
-                imageSwitcher.setImageResource(images[msg.arg1]);
                 imageSwitcher.setInAnimation(AnimationUtils.loadAnimation(getApplication(), R.anim.right_in));
-                imageSwitcher.setOutAnimation(AnimationUtils.loadAnimation(getApplication(), R.anim.left_out));
+                imageSwitcher.setImageResource(images[msg.arg1]);
             }
+            setImageBackground(msg.arg1);
         }
     };
 
 
     private void init(){
         imageSwitcher = (ImageSwitcher)findViewById(R.id.imageSwitcher);
+        linearLayout=(LinearLayout)findViewById(R.id.viewGroup);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_index);
         init();
 
         //ImageSwitcher初始化
         imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() { //设置工厂
             @Override
             public View makeView() { //重写makeView
-                ImageView imageView=new ImageView(MainActivity.this);
+                ImageView imageView=new ImageView(IndexActivity.this);
                 imageView.setBackgroundColor(0xFFFFFFFF); //白色背景
                 imageView.setScaleType(ImageView.ScaleType.FIT_CENTER); //居中显示
                 imageView.setLayoutParams(new ImageSwitcher.LayoutParams(
@@ -76,11 +77,31 @@ public class MainActivity extends Activity implements View.OnTouchListener{
                 R.drawable.img2,
                 R.drawable.img3
         };
+        imageSwitcher.setOnTouchListener(this);
+
+        tips=new ImageView[images.length];
+        for (int i=0;i<images.length;i++){
+            ImageView mImageView = new ImageView(this);
+            tips[i] = mImageView;
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(new ViewGroup.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT));
+            layoutParams.rightMargin = 10;
+            layoutParams.leftMargin = 10;
+
+            mImageView.setBackgroundResource(R.drawable.page_indicator_unfocused);
+            linearLayout.addView(mImageView, layoutParams);
+        }
+
+        index = getIntent().getIntExtra("position", 0);
+        imageSwitcher.setImageResource(images[index]);
+
+        setImageBackground(index);
+
 
         Runnable r=new Runnable() {
             @Override
             public void run() {
-                while (index <= images.length&&isSlip==false) {
+                while (index <= images.length) {
                     Message msg = new Message();
                     msg.what = 111;
                     msg.arg1 = index;
@@ -90,7 +111,7 @@ public class MainActivity extends Activity implements View.OnTouchListener{
                         index = 0;
                     }
                     try {
-                        Thread.sleep(2000); //暂停2秒继续，try…catch省略
+                        sleep(3000); //暂停3秒继续
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -98,8 +119,16 @@ public class MainActivity extends Activity implements View.OnTouchListener{
             }
         };
         new Thread(r).start();
+    }
 
-
+    private void setImageBackground(int selectItems){
+        for(int i=0; i<tips.length; i++){
+            if(i == selectItems){
+                tips[i].setBackgroundResource(R.drawable.page_indicator_focused);
+            }else{
+                tips[i].setBackgroundResource(R.drawable.page_indicator_unfocused);
+            }
+        }
     }
 
 
@@ -112,37 +141,39 @@ public class MainActivity extends Activity implements View.OnTouchListener{
             }
             case MotionEvent.ACTION_UP:{
                 float lastX = event.getX();
+                Message msg=new Message();
                 //抬起的时候的X坐标大于按下的时候就显示上一张图片
                 if(lastX > downX){
+                    msg.what=112;
                     if(index > 0){
                         index -- ;
                     }else{
                         index=images.length - 1;
                     }
-                    Message msg=new Message();
-                    msg.what=112;
-                    msg.arg1=index;
-                    handler.sendMessage(msg);
                 }
-
-                if(lastX < downX){
+                else if(lastX < downX){
+                    msg.what=113;
                     if(index < images.length - 1){
                         index ++ ;
                     }else{
                         index=0;
                     }
-                    Message msg=new Message();
-                    msg.what=113;
-                    msg.arg1=index;
-                    handler.sendMessage(msg);
                 }
+                msg.arg1=index;
+                handler.sendMessage(msg);
             }
-
             break;
         }
         return true;
     }
 
 
-
+    @Override
+    public View makeView() {
+        final ImageView i = new ImageView(this);
+        i.setBackgroundColor(0xff000000);
+        i.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        i.setLayoutParams(new ImageSwitcher.LayoutParams(RelativeLayout.LayoutParams.FILL_PARENT, RelativeLayout.LayoutParams.FILL_PARENT));
+        return i ;
+    }
 }
