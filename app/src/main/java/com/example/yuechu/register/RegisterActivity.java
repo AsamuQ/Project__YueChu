d066c5b5aa15a3a3732e3cd5f941d29ab4425697
Jo1ker <jiu2166312yongtao@163.com>
2018-07-18 10:43:52 +0800

    用户名为空可以注册问题解决

--- app/src/main/java/com/example/yuechu/register/RegisterActivity.java
+++ app/src/main/java/com/example/yuechu/register/RegisterActivity.java
@@ -58,8 +58,10 @@ public class RegisterActivity extends Activity {
 
         if (TextUtils.isEmpty(username)) {
             Toast.makeText(this, "用户名不能为空！", Toast.LENGTH_SHORT).show();
+
         } else if (TextUtils.isEmpty(password)) {
             Toast.makeText(this, "密码不能为空！", Toast.LENGTH_SHORT).show();
+
         }
 
         // 先判断改用户信息是否已经被注册
@@ -68,7 +70,7 @@ public class RegisterActivity extends Activity {
 
         if (person != null) {
             Toast.makeText(this, "该用户名已经被注册！", Toast.LENGTH_SHORT).show();
-        } else {
+        } else if(username.length()>0&&password.length()>0) {
             person = new Person(username, password, (short) 1);
             personDAO.add(person);
             Toast.makeText(this, "注册成功", Toast.LENGTH_SHORT).show();
