9fdf679e191108a9925c67d210f67ee6c34bf746
coiggahou <coiggahou@foxmail.com>
2018-07-19 09:38:44 +0800

    约厨模块已加入

---
+++ app/src/main/java/com/example/yuechu/page_yuechu/phb3Activity.java
@@ -0,0 +1,15 @@
+package com.example.yuechu.page_yuechu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.yuechu.R;
+
+public class phb3Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phb3);
+    }
+}
---
+++ app/src/main/java/com/example/yuechu/page_yuechu/startActivity.java
@@ -0,0 +1,68 @@
+package com.example.yuechu.page_yuechu;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+
+import com.example.yuechu.R;
+
+public class startActivity extends Activity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //隐藏标题栏
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        //隐藏状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_start);
+        initImage();
+    }
+
+    private void supportRequestWindowFeature(int featureNoTitle) {
+    }
+
+
+    private void initImage() {
+        ImageView  iv_start;
+        iv_start = (ImageView) findViewById(R.id.iv_start);
+        iv_start.setImageResource(R.drawable.start2);
+        //进行缩放动画
+        ScaleAnimation scaleAnimation = new ScaleAnimation(1.4f, 1.0f, 1.4f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scaleAnimation.setDuration(4000);
+        //动画播放完成后保持形状
+        scaleAnimation.setFillAfter(true);
+        scaleAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                //可以在这里先进行某些操作
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                start();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        iv_start.startAnimation(scaleAnimation);
+    }
+
+    private void start() {
+        Intent intent = new Intent(startActivity.this, ycActivity.class);
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        finish();
+    }
+}
